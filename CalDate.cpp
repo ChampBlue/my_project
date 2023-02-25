acbd692916d4113db49316e2fbab2e6fb27cdba7
ChampBlue <oliekim11@gmail.com>
2023-02-25 22:24:06 +0900

    calculate_date_ver1

---
+++ CalDate.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+
+using namespace std;
+
+class Data
+{
+    private:
+        int year_;
+        int month_;
+        int day_;
+
+    public:
+        void SetData(int year, int month, int date)
+        {
+            year_ = year;
+            month_ = month;
+            day_ = date;
+        }
+        void AddDay(int inc)
+        {
+            day_ = day_ + inc;
+            
+            if((month_ == 4 || month_ == 6 || month_ == 9 || month_ == 11) && day_ > 30) {
+                day_ = 1;
+                month_++;
+            } else if (month_ == 2 && day_ > 28) {
+                day_ = 1;
+                month_++;
+            } else if (day_ > 31) {
+                day_ = 1;
+                month_++;
+            }
+        }    
+            
+        void AddMonth(int inc)
+        {
+            month_ += inc;
+
+            if(month_ > 12) {
+                month_ = 1;
+                year_++;
+            }
+        }
+        void AddYear (int inc)
+        {
+            year_ += inc;
+        }
+        void ShowDate()
+        {
+            std::cout << year_ << "년" << month_ <<"월" << day_<<"일" << std::endl;
+        }
+};
+
+int main()
+{
+    Data data;
+    int inc;
+    int menu;
+    
+    while(1){
+        cout << endl;
+        cout <<endl;
+        cout << "menu" << endl;
+        cout << "1. setting data" << endl;
+        cout << "2. add day" << endl;
+        cout << "3. add month" << endl;
+        cout << "4. add year" << endl;
+        cout << "5. show data" << endl;
+        cout << "6. end" << endl;
+        cout <<endl;
+        cout <<endl;
+
+        cout << "input menu : " <<endl;
+        cin >> menu;
+        cout << endl;
+        cout << endl;
+
+        switch (menu)
+        {
+            case 1:
+                int year, month, date;
+
+                std::cout << "set today year : ";
+                std::cin >> year;
+                std::cout << "set today month : ";
+                std::cin >> month;
+                std::cout << "set today date : ";
+                std::cin >> date;
+
+                data.SetData(year, month, date);
+
+                break;
+
+            case 2:
+                cout << "input you want add date : ";
+                cin >> inc;
+                data.AddDay(inc);
+                break;
+
+            case 3:
+                cout << "input you want add month : ";
+                cin >> inc;
+                data.AddMonth(inc);
+                break;
+
+            case 4:
+                cout << "input you want add date : ";
+                cin >> inc;
+                data.AddYear(inc);
+                break;
+
+            case 5:
+                data.ShowDate();   
+                break;
+
+            case 6:
+                cout <<  "bye~~~!!" << endl;
+                exit(1);
+                        
+            default:
+                break;
+        }
+    }
+     
+    return 0;
+}
