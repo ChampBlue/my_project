1e7140f0ed8ea305baa4426f4e6b88cd5ccd9bd4
ChampBlue <oliekim11@gmail.com>
2023-04-03 15:52:32 +0900

    final_cam2

--- find_wrong_pictures/fianl_cam.py
+++ find_wrong_pictures/fianl_cam.py
@@ -1,10 +1,8 @@
-import os, time
-from PIL import ImageChops, Image
+import os
 import cv2
-import numpy as np
 import serial
 
-#sp = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+sp = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 
 webcam = cv2.VideoCapture(0)
 
@@ -27,19 +25,30 @@ while webcam.isOpened():
 
     if status:
         diff = cv2.absdiff(src, curr_frame)    
-        _, diff = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
+        _, diff = cv2.threshold(diff, 100, 255, cv2.THRESH_BINARY)
         contours, _ = cv2.findContours(diff, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         
         COLOR = (0, 200, 0)
-    
+        #count = 0
+        
         for cnt in contours:
-            if cv2.contourArea(cnt) > 10000:
+            if cv2.contourArea(cnt) > 5000:
                 x, y, width, height = cv2.boundingRect(cnt)
                 cv2.rectangle(frame, (x, y), (x + width, y + height), COLOR, 2)
-            cv2.imshow('chage', frame)
+                
+                if 0<=(x+width//2)<213:
+                    sp.write(('pan' + '45' + '\n').encode())
+                elif 213<=(x+width//2)<416:
+                    sp.write(('pan' + '90' + '\n').encode())
+                elif 416<=(x+width//2)<=640: 
+                    sp.write(('pan' + '135' + '\n').encode())
+            
+            cv2.imshow('change', frame)
             cv2.imshow('root', src)
             
-
+        #if cv2.waitKey(1) == 13:  # Enter 키를 눌렀을 때 이미지 저장
+            #cv2.imwrite("./detecting_diff_image/result_{}.jpg".format(count), frame)
+            
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
